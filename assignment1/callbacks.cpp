08d02b851c4dec8ed81af895a9ff136a7e9bd935
Till Rohrmann <till.rohrmann@googlemail.com>
2011-01-31 03:39:39 +0800

    animated koch curve finished

--- assignment1/callbacks.cpp
+++ assignment1/callbacks.cpp
@@ -1,6 +1,7 @@
 #include "callbacks.h"
 #include <iostream>
 #include <math.h>
+#include <ctime>
 #include <GL/glut.h>
 using namespace std;
 
@@ -10,6 +11,15 @@ double Drawer::translationX = 0;
 double Drawer::translationY = 0;
 double Drawer::rotation = 0;
 double Drawer::scale = 1;
+bool Drawer::animation =  true;
+bool Drawer::insertNewNodes = true;
+clock_t Drawer::lastTimeStamp = clock();
+int Drawer::level = 0;
+bool Drawer::deleteNodes = false;
+bool Drawer::increasePeaks = true;
+
+std::list<std::pair<double,double> > Drawer::nodes;
+std::list<std::pair<std::pair<double,double>,double > > Drawer::colors;
 
 void Drawer::drawKochCurveWrapper(){
 	instance.drawKochCurve();
@@ -23,10 +33,28 @@ void Drawer::init(){
 	glClearColor(1.0,1.0,1.0,1.0);
 	glShadeModel(GL_SMOOTH);
 
+	std::pair<double,double> node1(0,0.577350269);
+	std::pair<double,double> node2(-0.5,-0.288675135);
+	std::pair<double,double> node3(0.5,-0.288675135);
+
+	std::pair<double,double> colorH(0,1);
+	std::pair<std::pair<double,double>,double > color1(colorH,0);
+	std::pair<std::pair<double,double>,double > color2(colorH,0);
+	std::pair<std::pair<double,double>, double > color3(colorH,0);
+
+	nodes.push_back(node1);
+	nodes.push_back(node2);
+	nodes.push_back(node3);
+
+	colors.push_back(color1);
+	colors.push_back(color2);
+	colors.push_back(color3);
+
 	cout << "Move UP/DOWN: W/S" << endl;
 	cout << "Move LEFT/RIGHT: A/D" << endl;
 	cout << "Rotate clockwise/anti-clockwise: E/Q" << endl;
 	cout << "Zoom in/out: R/F" << endl;
+	cout << "Animation: T" << endl;
 }
 
 void Drawer::reshape(int width, int height){
@@ -48,7 +76,14 @@ void Drawer::drawKochCurve(){
 		glRotated(rotation,0,0,1);
 		glTranslated(translationX,translationY,0);
 
-		drawKochCurveHelper(5);
+		glBegin(GL_LINE_LOOP);
+		std::list<std::pair<std::pair<double,double>,double > >::const_iterator cit = colors.begin();
+		for(std::list<std::pair<double,double> >::const_iterator it = nodes.begin(); it != nodes.end(); ++it){
+			glColor3d(cit->first.first,cit->first.second,cit->second);
+			glVertex2d(it->first,it->second);
+			++cit;
+		}
+		glEnd();
 	glPopMatrix();
 	glFlush();
 }
@@ -90,8 +125,226 @@ void Drawer::drawKochCurveHelper(int i){
 	}
 }
 
+void Drawer::insertNodes(const std::pair<double,double>& first,const std::pair<double,double>& second,std::list<std::pair<double,double> >::iterator& it, std::list<std::pair<std::pair<double,double>,double > >::iterator& cit){
+	std::pair<double,double> n1;
+	std::pair<double,double> n2;
+	std::pair<double,double> n3;
+
+	std::pair<std::pair<double,double>,double > c1;
+	std::pair<std::pair<double,double>,double > c2;
+	std::pair<std::pair<double,double>,double > c3;
+
+	c1.first = std::pair<double,double>(0,1);
+	c1.second = 0;
+
+	c3 = c1;
+
+	c2.first = std::pair<double,double>(1,0);
+	c2.second = 0;
+
+	colors.insert(cit,c1);
+	colors.insert(cit,c2);
+	colors.insert(cit,c3);
+
+	double dx = second.first - first.first;
+	double dy = second.second - first.second;
+
+	n1.first = first.first + 1.0/3*dx;
+	n1.second = first.second +1.0/3*dy;
+
+	n2.first = first.first + 1.0/2*dx;
+	n2.second = first.second + 1.0/2*dy;
+
+	n3.first = first.first + 2.0/3*dx;
+	n3.second = first.second + 2.0/3*dy;
+
+	nodes.insert(it,n1);
+	nodes.insert(it,n2);
+	nodes.insert(it,n3);
+}
+
 void Drawer::idle(){
 
+	if(animation){
+		if(increasePeaks){
+			if(insertNewNodes && level < maxLevel){
+				std::list<std::pair<double,double> >::iterator it = nodes.begin();
+				std::pair<double,double> second = *it;
+				std::pair<double,double> first;
+				++it;
+
+				std::list<std::pair<std::pair<double,double>,double > >::iterator cit = colors.begin();
+				++cit;
+
+				while(it != nodes.end()){
+					first = second;
+					second = *it;
+					instance.insertNodes(first,second,it,cit);
+					++it;
+					++cit;
+				}
+
+				first = second;
+				second = nodes.front();
+
+				instance.insertNodes(first,second,it,cit);
+
+				insertNewNodes = false;
+
+				lastTimeStamp = clock();
+
+				level++;
+			}
+			else if(insertNewNodes && level >= maxLevel){
+				increasePeaks = false;
+				deleteNodes = false;
+			}
+			else{
+				clock_t nextTimeStamp = clock();
+				clock_t diff = nextTimeStamp -lastTimeStamp;
+				lastTimeStamp = nextTimeStamp;
+
+				std::list<std::pair<double,double> >::iterator it = nodes.begin();
+				std::list<std::pair<std::pair<double,double>,double > >::iterator cit = colors.begin();
+
+				while(it != nodes.end()){
+					++it;
+					std::pair<double,double>& first = *it;
+					++it;
+					std::pair<double,double>& second = *it;
+					++it;
+					std::pair<double,double>& third = *it;
+					++it;
+
+					++cit;
+					std::pair<std::pair<double,double>,double >&firstColor = *cit;
+					++cit;
+					std::pair<std::pair<double,double>,double >&secondColor = *cit;
+					++cit;
+					std::pair<std::pair<double,double>,double >&thirdColor = *cit;
+					++cit;
+
+					insertNewNodes = moveNode(first,second,third,firstColor,secondColor,thirdColor,diff);
+				}
+			}
+		}
+		else{
+			if(deleteNodes){
+				std::list<std::pair<double,double> >::iterator it = nodes.begin();
+				std::list<std::pair<std::pair<double,double>,double > >::iterator cit = colors.begin();
+
+				while(it != nodes.end()){
+					++it;
+					it = nodes.erase(it);
+					it = nodes.erase(it);
+					it = nodes.erase(it);
+					++cit;
+					cit = colors.erase(cit);
+					cit = colors.erase(cit);
+					cit = colors.erase(cit);
+				}
+				level--;
+				deleteNodes = false;
+
+				if(level == 0){
+					increasePeaks = true;
+				}
+			}
+			else{
+				clock_t nextTimeStamp = clock();
+				clock_t diff = nextTimeStamp -lastTimeStamp;
+				lastTimeStamp = nextTimeStamp;
+
+				std::list<std::pair<double,double> >::iterator it = nodes.begin();
+				std::list<std::pair<std::pair<double,double>,double > >::iterator cit = colors.begin();
+
+				while(it != nodes.end()){
+					++it;
+					std::pair<double,double>& first = *it;
+					++it;
+					std::pair<double,double>& second = *it;
+					++it;
+					std::pair<double,double>& third = *it;
+					++it;
+
+					++cit;
+					std::pair<std::pair<double,double>,double >&firstColor = *cit;
+					++cit;
+					std::pair<std::pair<double,double>,double >&secondColor = *cit;
+					++cit;
+					std::pair<std::pair<double,double>,double >&thirdColor = *cit;
+					++cit;
+
+					deleteNodes = unmoveNode(first,second,third,firstColor,secondColor,thirdColor,diff);
+				}
+			}
+
+		}
+		glutPostRedisplay();
+	}
+}
+
+bool Drawer::unmoveNode(std::pair<double,double> & first, std::pair<double,double>&second, std::pair<double,double>&third, std::pair<std::pair<double,double>,double >&firstColor, std::pair<std::pair<double,double>,double >&secondColor, std::pair<std::pair<double,double>,double >&thirdColor, clock_t diff){
+	double maxDistance = cos(1.0/6*M_PI)*sqrt((first.first-third.first)*(first.first-third.first) + (first.second-third.second)*(first.second-third.second));
+	double xMovement = third.second - first.second;
+	double yMovement = first.first - third.first;
+
+	second.first -= xMovement*diff*SPEED_MOVEX;
+	second.second -= yMovement*diff*SPEED_MOVEY;
+
+	double xMiddle = (first.first+third.first)/2;
+	double yMiddle = (first.second+third.second)/2;
+
+	double distance = sqrt((second.first-xMiddle)*(second.first-xMiddle)+(second.second-yMiddle)*(second.second-yMiddle));
+
+	secondColor.first.second = (1-distance/maxDistance);
+	secondColor.first.first = (distance/maxDistance);
+	double side = (xMovement*(second.first-xMiddle) + yMovement*(second.second-yMiddle));
+	if(side < 0){
+		second.first = xMiddle;
+		second.second = yMiddle;
+		return true;
+	}
+	else
+		return false;
+}
+
+bool Drawer::moveNode(std::pair<double,double> & first, std::pair<double,double>&second, std::pair<double,double>&third, std::pair<std::pair<double,double>,double >&firstColor, std::pair<std::pair<double,double>,double >&secondColor, std::pair<std::pair<double,double>,double >&thirdColor, clock_t diff){
+	double maxDistance = cos(1.0/6*M_PI)*sqrt((first.first-third.first)*(first.first-third.first) + (first.second-third.second)*(first.second-third.second));
+	double xMovement = third.second - first.second;
+	double yMovement = first.first - third.first;
+
+	second.first += xMovement*diff*SPEED_MOVEX;
+	second.second += yMovement*diff*SPEED_MOVEY;
+
+	double xMiddle = (first.first+third.first)/2;
+	double yMiddle = (first.second+third.second)/2;
+
+	double distance = sqrt((second.first-xMiddle)*(second.first-xMiddle)+(second.second-yMiddle)*(second.second-yMiddle));
+
+	secondColor.first.first = (1-distance/maxDistance);
+	secondColor.first.second = (distance/maxDistance);
+
+	if(distance > maxDistance){
+
+		double normalFac = sqrt(xMovement*xMovement + yMovement*yMovement);
+		second.first = xMovement*maxDistance/normalFac+xMiddle;
+		second.second = yMovement*maxDistance/normalFac+yMiddle;
+		return true;
+	}
+	else
+		return false;
+}
+
+void Drawer::printNodes(){
+	std::list<std::pair<std::pair<double, double>,double > >::const_iterator cit = colors.begin();
+	std::list<std::pair<double,double> >::const_iterator it = nodes.begin();
+	int i =0;
+	while(it != nodes.end()){
+		cerr <<i++ << ":(" << it->first << "," << it->second << ") RGB(" << cit->first.first << "," << cit->first.second << "," << cit->second << ")" << endl;
+		++it;
+		++cit;
+	}
 }
 
 /*
@@ -139,16 +392,19 @@ void Drawer::keyboard(unsigned char ch, int x, int y){
 		glutPostRedisplay();
 		break;
 	case 'f':
-		if(scale > SCALE){
-			scale /= SCALE;
-			glutPostRedisplay();
-		}
+		scale /= SCALE;
+		glutPostRedisplay();
+		break;
+	case 't':
+		animation = !animation;
+		lastTimeStamp = clock();
 		break;
 	}
 	cerr << "TranslationX:" << translationX << endl;
 	cerr << "TranslationY:" << translationY << endl;
 	cerr << "Scale:" << scale << endl;
 	cerr << "Rotation:" << rotation << endl;
+	cerr << "Animation:" << (animation ? "true" : "false") << endl;
 }
 
 
--- assignment1/callbacks.h
+++ assignment1/callbacks.h
@@ -8,25 +8,47 @@
 #ifndef CALLBACKS_H_
 #define CALLBACKS_H_
 
+#include <list>
+#include <utility>
+#include <ctime>
+
 class Drawer{
 private:
+	static const double SPEED_MOVEX=1.0/CLOCKS_PER_SEC;
+	static const double SPEED_MOVEY=1.0/CLOCKS_PER_SEC;
 	static const double SPEED_X=0.1;
 	static const double SPEED_Y=0.1;
 	static const double ROTATION=10;
 	static const double SCALE=1.1;
+	static const int maxLevel = 4;
 	static double translationX;
 	static double translationY;
 	static double rotation;
 	static double scale;
 	static Drawer instance;
+	static bool animation;
+	static bool insertNewNodes;
+	static bool deleteNodes;
+	static clock_t lastTimeStamp;
+	static int level;
+	static bool increasePeaks;
+
+	static std::list<std::pair<double,double> > nodes;
+	static std::list<std::pair<std::pair<double,double>, double > > colors;
+
+
 public:
 	static void drawKochCurveWrapper(void);
 	static void keyboard(unsigned char ch, int , int);
 	static void reshape(int width,int height);
 	static void idle();
 	static void init();
+	static void printNodes();
+	static bool unmoveNode(std::pair<double,double> & first, std::pair<double,double>&second, std::pair<double,double>&third, std::pair<std::pair<double,double>,double >&firstColor, std::pair<std::pair<double,double>,double >&secondColor, std::pair<std::pair<double,double>,double >&thirdColor, clock_t diff);
+	static bool moveNode(std::pair<double,double> & first, std::pair<double,double>&second, std::pair<double,double>&third, std::pair<std::pair<double,double>,double >&firstColor, std::pair<std::pair<double,double>,double >&secondColor, std::pair<std::pair<double,double>,double >&thirdColor, clock_t diff);
 	void drawKochCurve();
 	void drawKochCurveHelper(int i);
+	void insertNodes(const std::pair<double,double>& first, const std::pair<double,double>& second,std::list<std::pair<double,double> >::iterator& it, std::list<std::pair<std::pair<double,double>,double > >::iterator& cit);
 };
 
 
